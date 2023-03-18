c91a7b69c3fab4c879807f9a48eaaccfa415a1fc
sturmfx <ttunikitashtobert@gmail.com>
2023-03-18 13:48:57 +0200

    Add files via upload

---
+++ GameWindow.java
@@ -0,0 +1,13 @@
+package com.company;
+import javax.swing.*;
+
+public class GameWindow extends JFrame
+{
+    public GameWindow(String title)
+    {
+        setTitle(title);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setVisible(false);
+    }
+}
